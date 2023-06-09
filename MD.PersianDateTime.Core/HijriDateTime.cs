2db880c6390351746f053d9b3efd412c5f6a96be
Meysam Mahmoudi <meisamdev@gmail.com>
2023-06-09 12:15:59 +0330

    Simplify HijriDateTime

--- MD.PersianDateTime.Core/HijriDateTime.cs
+++ MD.PersianDateTime.Core/HijriDateTime.cs
@@ -1,28 +1,27 @@
-﻿namespace MD.PersianDateTime.Core
+﻿namespace MD.PersianDateTime.Core;
+
+/// <summary>
+/// تاریخ هجری قمری
+/// </summary>
+public struct HijriDateTime
 {
     /// <summary>
-    /// تاریخ هجری قمری
+    /// سال قمری
     /// </summary>
-    public struct HijriDateTime
-    {
-        /// <summary>
-        /// سال قمری
-        /// </summary>
-        public int Year { get; set; }
+    public int Year { get; set; }
 
-        /// <summary>
-        /// ماه قمری
-        /// </summary>
-	    public int Month { get; set; }
+    /// <summary>
+    /// ماه قمری
+    /// </summary>
+    public int Month { get; set; }
 
-        /// <summary>
-        /// نام ماه قمری
-        /// </summary>
-	    public string MonthName { get; set; }
+    /// <summary>
+    /// نام ماه قمری
+    /// </summary>
+    public string MonthName { get; set; }
 
-        /// <summary>
-        /// روز ماه قمری
-        /// </summary>
-	    public int Day { get; set; }
-    }
+    /// <summary>
+    /// روز ماه قمری
+    /// </summary>
+    public int Day { get; set; }
 }
